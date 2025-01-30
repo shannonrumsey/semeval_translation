fa28ed9496d01438ddc531155b11ab3467580c38
Darian-Lee-YTKA <136937505+Darian-Lee-YTKA@users.noreply.github.com>
2025-01-30 15:23:10 -0800

    fixed pretrain lengths

--- pretrain.py
+++ pretrain.py
@@ -7,6 +7,7 @@ import sentencepiece as spm
 from numpy.random import Generator, PCG64
 import sys
 
+
 # summary of changes 12/21:
 # pretrain now returns a dictionary of dfs for each language. This is needed to allow us to split after bpe in order to preserve sentence lengths
 
@@ -26,8 +27,10 @@ def get_data(lang):
         - AR, DE, ES have two components (["facebook/mlqa", ""mlqa-translate-train.lang_code"])
         - IT is small so the train and test split are combined
     """
-    lang_codes = {"KorQuAD/squad_kor_v2": "ko", "Thaweewat/instruct-qa-thai-combined": "th", "FurkyT/IMDB-Turkish-QA": "tr",
-                  "real-jiakai/chinese-squadv2": "zh", "qwant/squad_fr": "fr", "rajpurkar/squad": "en", "SkelterLabsInc/JaQuAD": "ja"}
+    lang_codes = {"KorQuAD/squad_kor_v2": "ko", "Thaweewat/instruct-qa-thai-combined": "th",
+                  "FurkyT/IMDB-Turkish-QA": "tr",
+                  "real-jiakai/chinese-squadv2": "zh", "qwant/squad_fr": "fr", "rajpurkar/squad": "en",
+                  "SkelterLabsInc/JaQuAD": "ja"}
     dfs = {}
 
     for l in lang:
@@ -53,8 +56,11 @@ def get_data(lang):
 
     return dfs
 
-lang = ["real-jiakai/chinese-squadv2", "FurkyT/IMDB-Turkish-QA", "Thaweewat/instruct-qa-thai-combined", "KorQuAD/squad_kor_v2",
-        "qwant/squad_fr", "squad_it", "rajpurkar/squad", "SkelterLabsInc/JaQuAD", ["facebook/mlqa", "mlqa-translate-train.ar"],
+
+lang = ["real-jiakai/chinese-squadv2", "FurkyT/IMDB-Turkish-QA", "Thaweewat/instruct-qa-thai-combined",
+        "KorQuAD/squad_kor_v2",
+        "qwant/squad_fr", "squad_it", "rajpurkar/squad", "SkelterLabsInc/JaQuAD",
+        ["facebook/mlqa", "mlqa-translate-train.ar"],
         ["facebook/mlqa", "mlqa-translate-train.de"], ["facebook/mlqa", "mlqa-translate-train.es"]]
 pretrain = get_data(lang)
 
@@ -67,6 +73,7 @@ for key, value in pretrain.items():
 # PREPARE FOR MUCH LESS PRETTY CODE XD
 # ==== we are loading the semeval data to do bpe on everything together ====
 base_dir = os.path.join(os.path.dirname(__file__), "data/semeval_train")
+
 
 def get_semeval_data(base_dir, for_bpe=False):
     # for_bpe allows user to specify what format the data should be in
@@ -82,26 +89,29 @@ def get_semeval_data(base_dir, for_bpe=False):
 
         if os.path.isfile(file_path):
             with open(file_path, "r", encoding="utf-8") as jsonl_file:
+                folder_name = file_name.split("_")[0]
+                print(folder_name)
+                if folder_name != ".DS":
+                    if for_bpe:
+                        data_target = [json.loads(line)["target"] for line in jsonl_file if
+                                       "target" in json.loads(line)]  # only gets the line with the translation
+                        data_source = [json.loads(line)["source"] for line in jsonl_file if
+                                       "source" in json.loads(line)]
+                        combined_data = data_target + data_source
 
-                if for_bpe:
-                    data_target = [json.loads(line)["target"] for line in jsonl_file if "target" in json.loads(line)] # only gets the line with the translation
-                    data_source = [json.loads(line)["source"] for line in jsonl_file if
-                                    "source" in json.loads(line)]
-                    combined_data = data_target + data_source
+                        df = pd.DataFrame({"text": combined_data})
 
-                    df = pd.DataFrame({"text": combined_data})
+                    else:
+                        data = [json.loads(line) for line in jsonl_file]
+                        df = pd.DataFrame(data)
 
-                else:
-                    data = [json.loads(line) for line in jsonl_file]
-                    df = pd.DataFrame(data)
-
-                semeval_train[folder_name] = df
+                    semeval_train[folder_name] = df
 
     # Get val datasets for the missing languages
     val_dir = os.path.join(os.path.dirname(__file__), "data/semeval_val")
     exceptions = ["ko_KR", "th_TH", "tr_TR", "zh_TW"]
     if os.path.isdir(val_dir):
-        for file_name in os.listdir(val_dir): 
+        for file_name in os.listdir(val_dir):
             base_name = os.path.splitext(file_name)[0]
             if base_name in exceptions:
 
@@ -109,7 +119,8 @@ def get_semeval_data(base_dir, for_bpe=False):
                 with open(json_file_path, "r", encoding="utf-8") as jsonl_file:
                     lines = list(jsonl_file)
                     if for_bpe:
-                        data_target = [json.loads(line)["targets"][0]["translation"] for line in lines if "targets" in json.loads(line)] # only gets the line with the translation
+                        data_target = [json.loads(line)["targets"][0]["translation"] for line in lines if
+                                       "targets" in json.loads(line)]  # only gets the line with the translation
                         data_source = [json.loads(line)["source"] for line in lines if "source" in json.loads(line)]
                         combined_data = data_target + data_source
                         df = pd.DataFrame({"text": combined_data})
@@ -117,14 +128,16 @@ def get_semeval_data(base_dir, for_bpe=False):
                     else:
                         data = [json.loads(line) for line in jsonl_file]
                         df = pd.DataFrame(data)
-                    
+
                     folder_name = base_name.split("_")[0]
                     semeval_train[folder_name] = df
 
     return semeval_train
 
+
 # data_by_language now has data for each language
 semeval_train = get_semeval_data(base_dir, for_bpe=True)
+
 
 def get_text_file_for_sentencepiece():
     # creating a text corpus for BPE
@@ -136,7 +149,6 @@ def get_text_file_for_sentencepiece():
 
         big_df = pd.concat([big_df, df], ignore_index=True)
     for key, value in pretrain.items():
-
         big_df = pd.concat([big_df, value], ignore_index=True)
 
     corpus = "\n".join(big_df["text"].dropna())
@@ -148,11 +160,13 @@ def get_text_file_for_sentencepiece():
 
 corpus = get_text_file_for_sentencepiece()
 
-
 # BPE
-# Removing dummy prefix correctly formats language codes
-spm.SentencePieceTrainer.train(input=corpus, model_prefix="tokenizer/tokenizer_combined", vocab_size=50000, add_dummy_prefix=False,
-                               character_coverage=0.9995, model_type="bpe")
+spm.SentencePieceTrainer.train(input=corpus, model_prefix="tokenizer/tokenizer_combined", vocab_size=50000,
+                               character_coverage=0.9995, model_type="bpe",
+                               user_defined_symbols=["</s>", "<es>", "<fr>", "<it>", "<de>", "<ar>", "<ja>", "<ko>",
+                                                     "<tr>", "<th>", "zh", "<en>"])
+sp = spm.SentencePieceProcessor(model_file="tokenizer/tokenizer_combined.model")
+
 
 def apply_bpe_tokenizer(df, column_name):
     sp = spm.SentencePieceProcessor(model_file="tokenizer/tokenizer_combined.model")
@@ -168,7 +182,7 @@ def get_chunks_from_corpuses(dataframes, chunk_size=35):
 
     Outputs:
             sampled_dfs: a dfs of text for each language where each row has the same sequence length
-            
+
     Notes: to reduce noise, we allow some repetiton by making sure the chunks always start at the beginning of a sentence
             example: if text is:
             "Toads have different distinctive features than what typically characterizes a frog. Often toads have drier, bumpier “warty” skin and prefer drier habitats. They usually have shorter hind limbs and rounder stouter bodies than most typical frogs. Toads have poison glands in their skin to keep predators from eating them and oftentimes produce a funny smell when handled."
@@ -186,22 +200,25 @@ def get_chunks_from_corpuses(dataframes, chunk_size=35):
         print(key)
         sub_word_df = apply_bpe_tokenizer(df, "text")
         sub_word_df["text"] = sub_word_df["text"].apply(lambda row: " ".join(row))
-        corpus = " ".join(sub_word_df["text"].dropna()) # join all the text in the df into one corpus so that chunking can occur
+        corpus = " ".join(
+            sub_word_df["text"].dropna())  # join all the text in the df into one corpus so that chunking can occur
 
         chunks = []
 
         # a loop for processing the corpus into chunks and appending them to the df
-        not_processed = corpus.split(" ") # a list of all the words in the corpus in order
+        not_processed = corpus.split(" ")  # a list of all the words in the corpus in order
         while len(not_processed) > chunk_size:
             current_chunk = not_processed[:chunk_size]
             if key[0] == "<":
-                chunks.append([key] + current_chunk) # some of the keys are in the form "en" and others are in the form "<en>"
+                chunks.append(
+                    [key] + current_chunk)  # some of the keys are in the form "en" and others are in the form "<en>"
             else:
                 chunks.append(["<" + key + ">"] + current_chunk)
 
             if "</s>" in current_chunk:
-                reversed_index = list(reversed(current_chunk)).index("</s>")  # reverse() followed by index() finds the last index of "</s>"
-                index = len(current_chunk) - reversed_index - 1 # unreversing it
+                reversed_index = list(reversed(current_chunk)).index(
+                    "</s>")  # reverse() followed by index() finds the last index of "</s>"
+                index = len(current_chunk) - reversed_index - 1  # unreversing it
                 not_processed = not_processed[index + 1:]
             else:
                 # print("No </s> found, using chunk size.")
@@ -211,7 +228,7 @@ def get_chunks_from_corpuses(dataframes, chunk_size=35):
         chunked_df = pd.DataFrame({"text": chunks})
         new_dfs[key] = chunked_df
 
-        lens[key] = len(chunks) # keep track of the lowest occuring class inorder to avoid class inbalances
+        lens[key] = len(chunks)  # keep track of the lowest occuring class inorder to avoid class inbalances
     print(lens)
     min_len = min(lens.values())
 
@@ -219,14 +236,15 @@ def get_chunks_from_corpuses(dataframes, chunk_size=35):
 
     return sampled_dfs
 
+
 pretrain = get_chunks_from_corpuses(pretrain, 35)
 
-sys.exit(0)
 print("after_chunking!!!")
 for key, df in pretrain.items():
     print(key)
     print(df.head())
     print("size: ", len(df))
+
 
 def noise(row, rng):
     """
@@ -252,18 +270,19 @@ def noise(row, rng):
 
     # Randomly select index to be masked, repeat this span_len times
     to_corrupt = rng.choice(text[1:], size=span_len, replace=False)
-    
+
     noisy_row = ["[MASK]" if word in to_corrupt else word for word in text]
 
     # Add lang code at the end to make shifted the same length as text
     print("printing text[0]")
     print([text[0]])
-    shifted = text[2:] + [text[0]]
+    shifted = text[1:] + [text[0]]
     # remove language code from beginning of decoder output
-    text = text[1:]
-    
+
+
     # move text over an indices to be decoder input
     return noisy_row, text, shifted
+
 
 rng = Generator(PCG64())
 # Randomly select span length (number of words to be masked)
